fc5267ee86e32f8ef4ae44f2b2ff2c63222bedb7
Mert Aydın <mert_aydin5@hotmail.com>
2017-07-09 03:11:36 +0300

    Project completed.- May need some improvements for other VOD URLs.

---
+++ CreateVideoFromHLS/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace CreateVideoFromHLS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(47, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(410, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "http://o1.tvod.stream/vod/b520ff8d-Zor-Sevda-49Bolum-Fragmani-7-Temmuz-Cuma.mp4/t" +
+    "racks-v1a1/index.m3u8";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "GET";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(47, 69);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(410, 287);
+            this.textBox2.TabIndex = 3;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(269, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "ESKİ DOSYALARI SİL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 378);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ CreateVideoFromHLS/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreateVideoFromHLS
+{
+    public partial class Form1 : Form
+    {
+        ArrayList tsFiles = new ArrayList();
+
+        public Form1()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {                
+            backgroundWorker1.RunWorkerAsync();        
+        }
+
+        private void doWork()
+        {
+            string m3u8File = textBox1.Text;
+            string fileNameWithoutM3u8 = m3u8File.Replace("index.m3u8", "");
+
+            var result = GetFileViaHttp(m3u8File.ToString());
+            string str = Encoding.UTF8.GetString(result);
+            string[] words = str.Split('\n');
+
+            for (Int64 i = 0; i < words.Length; i++)
+            {
+                string line = words[i].ToString() + "\n\n";
+
+                Regex r = new Regex(@"(.*)\.ts");
+
+                if (r.IsMatch(line.ToString()))
+                {
+                    MatchCollection myMatchCollection = r.Matches(line);
+                    string tsFileName = myMatchCollection[0].ToString();
+                    string tsFullPathFileName = fileNameWithoutM3u8 + tsFileName;
+                    tsFiles.Add(tsFileName);
+                    textBox2.AppendText(tsFileName + " indiriliyor..." + Environment.NewLine);
+                    downloadFile(tsFullPathFileName, tsFileName);                    
+                }
+                
+                label1.Text = i.ToString();
+            }
+
+            concatTsFiles(tsFiles);
+        }
+
+        public byte[] GetFileViaHttp(string url)
+        {
+            using (WebClient client = new WebClient())
+            {
+                return client.DownloadData(url);
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            doWork();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox2.ScrollBars = ScrollBars.Both;
+        }
+
+        private void downloadFile(string fullPath, string fileName)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                wc.DownloadFile(new System.Uri(fullPath), fileName);
+            }
+        }
+
+        static void FFmpegConversion(string command)
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = "ffmpeg\\bin\\ffmpeg.exe";
+            proc.StartInfo.Arguments = command.ToString();
+            proc.StartInfo.RedirectStandardError = true;
+            proc.StartInfo.UseShellExecute = false;
+
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.CreateNoWindow = true;
+
+            if (!proc.Start())
+            {
+                Console.WriteLine("Error!");
+                return;
+            }
+            StreamReader reader = proc.StandardError;
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+            proc.Close();
+        }
+
+        static void concatTsFiles(ArrayList tsFiles)
+        {
+            string concatString = "";
+            string ffmpegCommand = "";
+            foreach (string file in tsFiles)
+            {
+                concatString = concatString + file + "|";
+            }
+            concatString = concatString.Remove(concatString.Length - 1, 1);
+            ffmpegCommand = "-i \"concat:" + concatString + "\" -c copy output.ts";
+            Console.WriteLine(ffmpegCommand);
+            FFmpegConversion(ffmpegCommand);
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            textBox2.AppendText("BİTTİ!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (string file in tsFiles)
+            {
+                if (Directory.Exists(Path.GetDirectoryName(file)))
+                {
+                    File.Delete(file);
+                }
+            }
+        }
+    }
+}
